c84a2e707d40b730ac90a3c18d8ccc0b798b0ebd
TECHandGUARD <aneleqamata95@gmail.com>
2026-06-15 02:02:03 +0200

    Add staleTime and gcTime options to QueryClient

--- src/lib/query-client.js
+++ src/lib/query-client.js
@@ -1,10 +1,14 @@
 import { QueryClient } from '@tanstack/react-query';
 
-
 export const queryClientInstance = new QueryClient({
 	defaultOptions: {
 		queries: {
 			refetchOnWindowFocus: false,
+			retry: 1,
+			staleTime: 1000 * 60 * 5, // 5 minutes - data considered fresh
+			gcTime: 1000 * 60 * 10, // 10 minutes - garbage collection time (React Query v5)
+		},
+		mutations: {
 			retry: 1,
 		},
 	},
